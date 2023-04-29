648dcaddc7ace1d838be1d2ff5331bccd0da30c2
Joe-metry <joemetry150@gmail.com>
2023-04-29 13:58:36 +0100

    Method 2 for divisibility test

---
+++ divisibilityMethod2.js
@@ -0,0 +1,10 @@
+// Second and lengthy approach
+
+Funct = (n, x, y) => {
+    const a = n % x;
+    const b = n % y;
+    if (a === 0 && b === 0) {
+        return "n is divisible by both x and y";
+    }
+    return " Try other inputs";
+}
